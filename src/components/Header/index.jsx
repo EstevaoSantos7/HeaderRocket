76679405da6be08f2cd589b2c238865d1f865b10
EstevaoSantos7 <since1178@gmail.com>
2025-04-04 15:54:54 -0300

    first

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import style from './style.module.css'
+import { FiAlignJustify } from "react-icons/fi";
+
+export default function Header() {
+return(
+<div className={style.headerCima}>
+    <div className={style.containerHeader}>
+<nav>
+<FiAlignJustify size={50} style={{color:"white"}} />
+
+    <a href=""></a>
+<a href=""></a>
+
+</nav>
+    </div>
+</div>
+
+)
+
+}
